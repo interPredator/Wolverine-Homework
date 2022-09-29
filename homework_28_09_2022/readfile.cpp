a09dad39e4ad33c3b9417dcc688a9c512c099223
Sevak Tadevosyan <102533871+interPredator@users.noreply.github.com>
2022-09-29 18:37:55 +0400

    Update readfile.cpp

--- homework_28_09_2022/readfile.cpp
+++ homework_28_09_2022/readfile.cpp
@@ -10,7 +10,7 @@ int main() {
 	if (!iFile.is_open()) { std::cout << "File is not opened"; return 0; }
 	while (getline(iFile, str)) {
 		i = 0;
-		while (str[i] != '\0') {
+		while (str[i]) {
 			if (isChecked(str[i]) && !isChecked(str[i + 1])) {
 				std::cout << str[i] << std::endl;
 			} else if (isChecked(str[i])) {
